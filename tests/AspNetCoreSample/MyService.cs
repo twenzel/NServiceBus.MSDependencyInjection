8d5ec7db1e9134df1f2af2cc88562552fce77bbb
Wenzel, Toni <toni.wenzel@googlemail.com>
2018-09-04 11:27:39 +0200

    Add Sample project

---
+++ tests/AspNetCoreSample/MyService.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus.Logging;
+
+namespace Sample.Core
+{
+    public class MyService
+    {
+        static ILog log = LogManager.GetLogger<MyService>();
+
+        public void WriteHello()
+        {
+            log.Info("Hello from MyService.");
+        }
+    }
+}
